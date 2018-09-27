905a28ae737d666a97273d3a29693af54aaf97b7
chadh <chadh>
2018-09-27 14:02:50 -0400

    quick module ucd

---
+++ terraform/jke-via-module-test/README.md
@@ -0,0 +1,8 @@
+## jke-via-module-test infrastructure
+
+Created 9/27/18 by ucdpadmin. For Cloud AWS-SPB for jke-via-module-test
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ terraform/jke-via-module-test/variables.tf
@@ -0,0 +1,61 @@
+#####################################################################
+##
+##      Created 9/27/18 by ucdpadmin. For Cloud AWS-SPB for jke-via-module-test
+##
+#####################################################################
+
+variable "modweb1_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "modweb1_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "modweb1_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "modweb2_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "modweb2_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "group_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vpc_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "moddb_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "moddb-user" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "modweb1-user" {
+  type = "string"
+  description = "Generated"
+}
+
