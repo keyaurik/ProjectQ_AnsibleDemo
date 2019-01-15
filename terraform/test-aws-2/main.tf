2febcf3645fc9448d12db3e233de9e87641d3048
chadh <chadh>
2019-01-15 13:50:19 -0500

    test

--- terraform/test-aws-2/main.tf
+++ terraform/test-aws-2/main.tf
@@ -59,6 +59,34 @@ EOT
   }
 }
 
+resource "aws_instance" "jke-db" {
+  ami = "${var.jke-db_ami}"
+  key_name = "${aws_key_pair.auth.id}"
+  instance_type = "${var.jke-db_aws_instance_type}"
+  availability_zone = "${var.availability_zone}"
+  connection {
+    user = "centos"
+    private_key = "${tls_private_key.ssh.private_key_pem}"  # tls_private_key
+  }
+  provisioner "ucd" {
+    agent_name      = "${var.jke-db_agent_name}.${random_id.jke-db_agent_id.dec}"
+    ucd_server_url  = "${var.ucd_server_url}"
+    ucd_user        = "${var.ucd_user}"
+    ucd_password    = "${var.ucd_password}"
+  }
+  provisioner "local-exec" {
+    when = "destroy"
+    command = <<EOT
+    curl -k -u ${var.ucd_user}:${var.ucd_password} ${var.ucd_server_url}/cli/agentCLI?agent=${var.jke-db_agent_name}.${random_id.jke-db_agent_id.dec} -X DELETE
+EOT
+}
+  subnet_id  = "${data.aws_subnet.subnet.id}"
+  vpc_security_group_ids = ["${data.aws_security_group.group_name.id}"]
+  tags {
+    Name = "${var.jke-db_name}"
+  }
+}
+
 resource "tls_private_key" "ssh" {
     algorithm = "RSA"
 }
@@ -74,7 +102,29 @@ resource "ucd_component_mapping" "WebSphere_Liberty_Profile" {
   parent_id = "${ucd_agent_mapping.jke-web_agent.id}"
 }
 
+resource "ucd_component_mapping" "jke_war" {
+  component = "jke.war"
+  description = "jke.war Component"
+  parent_id = "${ucd_agent_mapping.jke-web_agent.id}"
+}
+
+resource "ucd_component_mapping" "MySQL_Server" {
+  component = "MySQL Server"
+  description = "MySQL Server Component"
+  parent_id = "${ucd_agent_mapping.jke-db_agent.id}"
+}
+
+resource "ucd_component_mapping" "jke_db" {
+  component = "jke.db"
+  description = "jke.db Component"
+  parent_id = "${ucd_agent_mapping.jke-db_agent.id}"
+}
+
 resource "random_id" "jke-web_agent_id" {
+  byte_length = 8
+}
+
+resource "random_id" "jke-db_agent_id" {
   byte_length = 8
 }
 
@@ -83,6 +133,32 @@ resource "ucd_component_process_request" "WebSphere_Liberty_Profile" {
   environment = "${ucd_environment.environment.id}"
   process = "deploy"
   resource = "${ucd_component_mapping.WebSphere_Liberty_Profile.id}"
+  version = "LATEST"
+}
+
+resource "ucd_component_process_request" "jke_war" {
+  depends_on = [ "ucd_component_process_request.jke_db" ]
+  component = "jke.war"
+  environment = "${ucd_environment.environment.id}"
+  process = "deploy"
+  resource = "${ucd_component_mapping.jke_war.id}"
+  version = "LATEST"
+}
+
+resource "ucd_component_process_request" "MySQL_Server" {
+  component = "MySQL Server"
+  environment = "${ucd_environment.environment.id}"
+  process = "deploy mysql 5.7"
+  resource = "${ucd_component_mapping.MySQL_Server.id}"
+  version = "LATEST"
+}
+
+resource "ucd_component_process_request" "jke_db" {
+  depends_on = [ "ucd_component_process_request.MySQL_Server", "ucd_component_process_request.WebSphere_Liberty_Profile" ]
+  component = "jke.db"
+  environment = "${ucd_environment.environment.id}"
+  process = "deploy"
+  resource = "${ucd_component_mapping.jke_db.id}"
   version = "LATEST"
 }
 
@@ -100,11 +176,30 @@ resource "ucd_environment" "environment" {
       value = "testme"
       secure = false
   }
+  component_property {
+      component = "jke.war"
+      name = "JKE_DB_HOST"
+      value = "${aws_instance.jke-db.public_ip}"  # aws_instance
+      secure = false
+  }
+  component_property {
+      component = "jke.db"
+      name = "ChadPropEnv"
+      value = "default"
+      secure = false
+  }
 }
 
 resource "ucd_agent_mapping" "jke-web_agent" {
   depends_on = [ "aws_instance.jke-web" ]
   description = "Agent to manage the jke-web server"
   agent_name = "${var.jke-web_agent_name}.${random_id.jke-web_agent_id.dec}"
+  parent_id = "${ucd_resource_tree.resource_tree.id}"
+}
+
+resource "ucd_agent_mapping" "jke-db_agent" {
+  depends_on = [ "aws_instance.jke-db" ]
+  description = "Agent to manage the jke-db server"
+  agent_name = "${var.jke-db_agent_name}.${random_id.jke-db_agent_id.dec}"
   parent_id = "${ucd_resource_tree.resource_tree.id}"
 }
--- terraform/test-aws-2/variables.tf
+++ terraform/test-aws-2/variables.tf
@@ -7,36 +7,43 @@
 variable "jke-web_ami" {
   type = "string"
   description = "Generated"
+  default = "ami-759bc50a"
 }
 
 variable "jke-web_aws_instance_type" {
   type = "string"
   description = "Generated"
+  default = "t2.medium"
 }
 
 variable "availability_zone" {
   type = "string"
   description = "Generated"
+  default = "us-east-1a"
 }
 
 variable "jke-web_name" {
   type = "string"
   description = "Generated"
+  default = "jkeweb55"
 }
 
 variable "aws_key_pair_name" {
   type = "string"
   description = "Generated"
+  default = "key55"
 }
 
 variable "vpc_id" {
   type = "string"
   description = "Generated"
+  default = "vpc-6c51be09"
 }
 
 variable "group_name" {
   type = "string"
   description = "Generated"
+  default = "ucdev_secgroup_nva"
 }
 
 variable "ucd_user" {
@@ -59,11 +66,37 @@ variable "ucd_server_url" {
 variable "environment_name" {
   type = "string"
   description = "Environment name"
+  default = "env55"
 }
 
 variable "jke-web_agent_name" {
   type = "string"
   description = "Agent name"
+  default = "agent55"
+}
+
+variable "jke-db_ami" {
+  type = "string"
+  description = "Generated"
+  default = "ami-b81dbfc5"
+}
+
+variable "jke-db_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+  default = "t2.medium"
+}
+
+variable "jke-db_name" {
+  type = "string"
+  description = "Generated"
+  default = "db55"
+}
+
+variable "jke-db_agent_name" {
+  type = "string"
+  description = "Agent name"
+  default = "dbagent55"
 }
 
 
