824d36ee0d898cc9ac777b4a16872b315a0b5e89
chadh <chadh>
2019-06-14 16:36:47 +0000

    Add demo xyz

---
+++ demo-xyz/README.md
@@ -0,0 +1,8 @@
+## demo-xyz infrastructure
+
+Created 6/14/19 by ucdpadmin for cloud cmh-aws. for demo-xyz
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ demo-xyz/variables.tf
@@ -0,0 +1,46 @@
+#####################################################################
+##
+##      Created 6/14/19 by ucdpadmin for cloud cmh-aws. for demo-xyz
+##
+#####################################################################
+
+variable "my-vm_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "my-vm_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "my-vm_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_key_pair_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "vpc_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "myvol_volume_size" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "group_name" {
+  type = "string"
+  description = "Generated"
+}
+
