9f5c8c25635df5fe436778b86f04ac8b5368903e
chadh <chadh>
2019-10-01 14:31:54 -0400

    Add AWS 2-node example

---
+++ terraform/aws-two-node/outputs.tf
@@ -0,0 +1,34 @@
+#####################################################################
+##
+##      Created 10/1/19 by ucdpadmin for cloud aws-brad. for aws-two-node
+##
+#####################################################################
+
+output "camtags_tagsmap" {
+  value = "${module.camtags.tagsmap}"
+}
+
+output "camtags_tagslist" {
+  value = "${module.camtags.tagslist}"
+}
+
+# Web server IP address
+output "web_server_ip_address" {
+  value = "${aws_instance.web-server.public_ip}"
+}
+
+# DB server IP address
+output "db_server_ip_address" {
+  value = "${aws_instance.db-server.public_ip}"
+}
+
+# Encoded TLS private key
+output "tls-private-key" {
+  value = "${base64encode(tls_private_key.ssh.private_key_pem)}"
+}
+
+# Centos AMI user
+output "web-server-user" {
+  value = "${var.centos-user}"
+}
+
